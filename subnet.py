de47d171d81450de56e9a9de4fd447c94a4a3153
msingh1002 <ms15355@nyu.edu>
2024-11-02 22:56:02 -0400

    Add files via upload

--- subnet.py
+++ subnet.py
@@ -20,34 +20,34 @@ p3First = '175.219.207.177'
 p3Last = '175.219.207.190'
 
 #problem 4
-p4Network = ''
-p4Broadcast = ''
-p4First = ''
-p4Last = ''
+p4Network = '86.205.168.192'
+p4Broadcast = '86.205.168.255'
+p4First = '86.205.168.193'
+p4Last = '86.205.168.255'
 
 #problem 5
-p5Network = ''
-p5Broadcast = ''
-p5First = ''
-p5Last = ''
+p5Network = '253.190.130.16'
+p5Broadcast = '253.190.130.23'
+p5First = '253.190.130.17'
+p5Last = '253.190.130.22'
 
 #problem 6
-p6Network = ''
-p6Broadcast = ''
-p6First = ''
-p6Last =  ''
+p6Network = '164.175.92.32'
+p6Broadcast = '164.175.92.33'
+p6First = '164.175.92.33'
+p6Last =  '164.175.92.62'
 
 #problem 7
-p7Network = ''
-p7Broadcast = ''
-p7First = ''
-p7Last = ''
+p7Network = '75.160.54.0'
+p7Broadcast = '75.160.54.255'
+p7First = '75.160.54.1'
+p7Last = '75.160.54.254'
 
 #problem 8
-p8Network = ''
-p8Broadcast = ''
-p8First = ''
-p8Last = ''
+p8Network = '242.145.15.144'
+p8Broadcast = '242.145.15.159'
+p8First = '242.145.15.145'
+p8Last = '242.145.15.158'
 
 #problem 9
 p9Network = '153.130.233.128'
