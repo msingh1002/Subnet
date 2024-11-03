d28894fbd7a278a4c96f7f1ed5e699d4d2cee8b0
msingh1002 <ms15355@nyu.edu>
2024-11-02 22:00:05 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 9143
+
+#problem 1
+p1Network = '97.249.27.103'
+p1Broadcast = '97.249.27.127'
+p1First = '97.249.27.97'
+p1Last =  '97.249.27.126'
+
+#problem 2
+p2Network = '8.234.245.145'
+p2Broadcast = '8.234.245.128'
+p2First = '8.234.245.129'
+p2Last = '8.234.245.255'
+
+#problem 3
+p3Network = '175.219.207.176'
+p3Broadcast = ''
+p3First = ''
+p3Last = ''
+
+#problem 4
+p4Network = ''
+p4Broadcast = ''
+p4First = ''
+p4Last = ''
+
+#problem 5
+p5Network = ''
+p5Broadcast = ''
+p5First = ''
+p5Last = ''
+
+#problem 6
+p6Network = ''
+p6Broadcast = ''
+p6First = ''
+p6Last =  ''
+
+#problem 7
+p7Network = ''
+p7Broadcast = ''
+p7First = ''
+p7Last = ''
+
+#problem 8
+p8Network = ''
+p8Broadcast = ''
+p8First = ''
+p8Last = ''
+
+#problem 9
+p9Network = ''
+p9Broadcast = ''
+p9First = ''
+p9Last = ''
+
+#problem 10
+p10Network = ''
+p10Broadcast = ''
+p10First = ''
+p10Last = ''
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
